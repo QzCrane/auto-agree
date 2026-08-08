ac5cf9c1bd01d77858d67c9e9b31fbaf7db0e7f4
QzCrane <107861762+QzCrane@users.noreply.github.com>
2026-08-09 02:11:57 +0800

    feat: add cooperative generation click lease

---
+++ extension/generation-lease.js
@@ -0,0 +1,29 @@
+(() => {
+  'use strict';
+  const VERSION = '10.0.0';
+  if (globalThis.__AUTO_AGREE_GENERATION_LEASE__?.version === VERSION) return;
+
+  const descriptor = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'click');
+  const nativeClick = descriptor?.value;
+  if (typeof nativeClick !== 'function') throw new Error('Auto Agree generation lease cannot resolve HTMLElement.click');
+
+  function current() {
+    try {
+      return chrome.runtime?.getManifest?.()?.version === VERSION;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  function guardedClick(...args) {
+    if (!current()) return undefined;
+    return Reflect.apply(nativeClick, this, args);
+  }
+
+  Object.defineProperty(HTMLElement.prototype, 'click', {
+    ...descriptor,
+    value: guardedClick
+  });
+
+  globalThis.__AUTO_AGREE_GENERATION_LEASE__ = Object.freeze({ version: VERSION, current });
+})();
